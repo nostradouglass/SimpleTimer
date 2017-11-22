61102f0f56c2917d4823455ae2301085a8aa7dd0
Kelly Douglass <kdouglass@gmail.com>
2017-11-21 22:20:55 -0600

    Finished Simple Timer

--- TimerKelly/ViewController.swift
+++ TimerKelly/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var number = 60
+	
+	var timer = Timer()
+	
+	@IBOutlet weak var counterLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		counterLabel.text = String(number)
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +29,38 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@objc func decreasebyOne() {
+		if number > 0 {
+		number -= 1
+		counterLabel.text = String(number)
+		} else {
+			timer.invalidate()
+		}
+	}
+	
+	@IBAction func startPresses(_ sender: Any) {
+			timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decreasebyOne), userInfo: nil, repeats: true)
+		
+	}
+	
+	@IBAction func pausePressed(_ sender: Any) {
+		timer.invalidate()
+	}
+	
+	@IBAction func resetPresses(_ sender: Any) {
+		
+		number = 60
+		counterLabel.text = String(number)
+	}
+	
+	@IBAction func subrtractPressed(_ sender: Any) {
+		number -= 10
+	}
+	
+	@IBAction func addPressed(_ sender: Any) {
+		if number < 50 {
+			number += 10
+		}
+	}
 }
 
