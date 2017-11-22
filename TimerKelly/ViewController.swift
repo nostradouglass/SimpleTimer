09c4b68ef76499424e97267aaf6326cfdd9a1eb7
Kelly Douglass <kdouglass@gmail.com>
2017-11-21 21:15:14 -0600

    Initial Commit

---
+++ TimerKelly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerKelly
+//
+//  Created by Kelly Douglass on 11/21/17.
+//  Copyright © 2017 Kelly Douglass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
